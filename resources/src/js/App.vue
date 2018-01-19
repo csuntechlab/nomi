2e9cbff8a215f4580f8bcd8f93f0d05258a9b27c
FirstProject <jackkiey24@gmail.com>
2018-01-19 14:46:10 -0800

    Created a rough grid for the first time.

---
+++ resources/src/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+	
+    <div class="grid-container">
+        <div class="box1 grid-item">1</div>
+        <div class="box2 grid-item">2</div>
+        <div class="box1 grid-item">3</div>
+    </div>
+  <!--   <div class="box">hello</div> -->
+
+</div>
+</template> <!-- end of template  -->
+
+<script>
+    export default {
+        
+    }
+</script>
