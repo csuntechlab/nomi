0c8fb7ac41cda9b5a523460c7f9938f6ad6fb371
FirstProject <jackkiey24@gmail.com>
2018-01-24 16:34:25 -0800

    Fixed the grid container bug.

--- resources/src/js/App.vue
+++ resources/src/js/App.vue
@@ -25,9 +25,9 @@ export default {
 			farmAnimals: [
 				{ animal: "Pig", image: "https://kids.nationalgeographic.com/content/dam/kids/photos/animals/Mammals/H-P/pig-young-closeup.ngsversion.1412640764383.jpg" },
 	    		{ animal: "Chicken", image: "http://www.newstalk.com/content/000/images/000129/133205_54_news_hub_123495_656x500.jpg" },
-	    		{ animal: "Duck", image: "https://web.stanford.edu/dept/CTL/cgi-bin/academicskillscoaching/wp-content/uploads/2012/07/baby-duck.jpg" },
 	    		{ animal: "Rooster", image: "https://bloximages.newyork1.vip.townnews.com/roanoke.com/content/tncms/assets/v3/editorial/0/1f/01fb8392-9599-54ef-a072-eccba5ef5a9b/588964584fea3.image.jpg?resize=1200%2C771" },
-	    		{ animal: "Cow", image: "https://cdn.modernfarmer.com/wp-content/uploads/2015/08/cowburp.jpeg" }
+	    		{ animal: "Cow", image: "https://cdn.modernfarmer.com/wp-content/uploads/2015/08/cowburp.jpeg" },
+	    		{ animal: "Duck", image: "https://web.stanford.edu/dept/CTL/cgi-bin/academicskillscoaching/wp-content/uploads/2012/07/baby-duck.jpg" },
 	    		
 			],
 			
