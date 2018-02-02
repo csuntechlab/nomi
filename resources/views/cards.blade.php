9bc0472907c8144c6053df92b47f1bce36c4ff1b
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2018-02-02 12:50:15 -0800

    moved liad's links to the main page

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <card-images></card-images>
+    <br>
+    <br>
+    <ul>
+        <li><a href='{!! url('/courses/2173'); !!}'>Course List</a></li>
+        <li><a href='{!! url('/students/2173/0') !!}'>Student List</a></li>
+        <li><a href='{!! url('students/shuffle/2173/0') !!}'>Shuffled Students</a></li>
+    </ul>
 @stop
