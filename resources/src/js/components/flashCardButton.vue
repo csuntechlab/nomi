e5de734cbf32cab394152f85332df65676377a9a
mrci18 <charles.ibo.247@my.csun.edu>
2018-02-07 17:24:08 -0800

    Button to toggle hiding name and image

---
+++ resources/src/js/components/flashCardButton.vue
@@ -0,0 +1,22 @@
+<!--<template>
+<button @click="flashCard" role="button" class="btn btn-default">Button</button>
+</div>
+</template>
+
+<script>
+export default{
+    methods: {
+        flashCard: function() {
+            //		    grabs id of image
+            var imageId = document.getElementById("myDiv");
+            var textId = document.getElementById("textA");
+            if (imageId.style.display !== "none") {
+                imageId.style.display = "none";
+                textId.style.display = "block";
+            } else {
+                imageId.style.display = "block";
+                textId.style.display = "none";
+            }
+    }
+}
+</script>-->
