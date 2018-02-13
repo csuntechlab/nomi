6c8640a8e75070cacebac613a3b3238f262e8220
FirstProject <jackkiey24@gmail.com>
2018-02-13 13:55:13 -0800

    Fix bug

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <shuffle-button></shuffle-button>
     <card-images students="{{json_encode($students)}}"></card-images>
-<<<<<<< HEAD
     <br>
     <br>
     <ul>
@@ -12,6 +11,4 @@
         <li><a href='{!! url('students/shuffle/2173/0') !!}'>Shuffled Students</a></li>
         <li><a href='{!! url('logout') !!}'>Logout</a></li>
     </ul>   
-=======
->>>>>>> 30fbb6dc6a3992e63fcf4a5f1f2bb30a7c5f1b60
 @stop
