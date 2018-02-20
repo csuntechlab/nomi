ab608668ea070aeff1a8d05e02070d6bbc517b49
Liad Golan <liad.golan.736@my.csun.edu>
2018-02-20 12:36:46 -0800

    started studentListItem component

---
+++ resources/src/js/components/studentListItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "student-list-item",
+
+        props:['student']
+    }
+</script>
+
+<style>
+
+</style>
