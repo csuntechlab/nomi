9f9835ad6e5976993e143e9e0fab05d9224cd375
FirstProject <jackkiey24@gmail.com>
2018-03-09 10:20:36 -0800

    Fix bugs

--- public/js/app.js
+++ public/js/app.js
@@ -18314,10 +18314,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-//
-//
-//
-//
 
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,8 +4,8 @@
 <body class="bg--gray">
 <div class="container">
     <router-view json="{{json_encode($json)}}"></router-view>
-    <side-bar></side-bar>
-    <menu-bar></menu-bar>
+    <!-- <side-bar></side-bar> -->
 </div>
+<menu-bar></menu-bar>
 </body>
 @stop
