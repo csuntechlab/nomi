073c6c6708dcee132a6c73bd1cda3d6825debc67
FirstProject <jackkiey24@gmail.com>
2018-03-13 13:09:42 -0700

    Create sidebar button

---
+++ resources/src/js/components/fixed_components/sideBarButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="sidebar==false">
+        <button @click="emitToggleSideBar()">
+            
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "side-bar-button",
+
+        data: function () {
+            return {
+                sidebar: false
+            }
+        },
+
+        methods: {
+            emitToggleSideBar: function () {
+                this.$eventBus.$emit('ToggleSideBar');
+                this.sidebar = !this.sidebar;
+            }
+        }
+    }
+</script>
