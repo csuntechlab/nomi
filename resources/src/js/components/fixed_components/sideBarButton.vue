b2c72d613ddeeb5da872214caf4802a2d1f89516
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2018-04-13 13:28:26 -0700

    Revert "Initial commit after IDE changes"
    This reverts commit 0f6c552ec45663a0128b72a4eb53223bebc08e5f.

---
+++ resources/src/js/components/fixed_components/sideBarButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <i class="fa fa-angle-up fa-3x" id="caret" title="Menu" @click.prevent="openNav()"></i>
+</template>
+
+<script>
+    export default {
+        name: "side-bar-button",
+        methods: {
+             openNav: function() {
+                document.getElementById("sideNav").style.height = "75%";
+            }
+        }
+    }
+</script>
