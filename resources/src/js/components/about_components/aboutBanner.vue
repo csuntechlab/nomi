72d3856756d3fe97a175eab8d371727726557090
Liad Golan <liad.golan.736@my.csun.edu>
2018-05-16 11:54:58 -0700

    sets up the tabs for the about page

---
+++ resources/src/js/components/about_components/aboutBanner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="nav-bars">
+        <div class="tab-container">
+            <ul class="tabs cf type--center">
+                <li class="tab__list">
+                    <a class="tab__link tab__link--active" href="#">About</a>
+                </li>
+                <li class="tab__list">
+                    <a class="tab__link" href="#">Version History</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "about-banner",
+
+    }
+</script>
--- resources/src/js/views/about.vue
+++ resources/src/js/views/about.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
+        <about-banner></about-banner>
         <h1>What is NOMI?</h1>
     </div>
 </template>
 
 <script>
+    import AboutBanner from "../components/about_components/aboutBanner";
+
     export default {
+        components: {AboutBanner},
         name: "class"
     }
 </script>
