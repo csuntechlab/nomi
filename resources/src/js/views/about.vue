abdab8bd28207b639ae489659348cbee45660c98
Liad Golan <liad.golan.736@my.csun.edu>
2018-05-16 16:31:55 -0700

    finished about page

--- resources/src/js/views/about.vue
+++ resources/src/js/views/about.vue
@@ -1,20 +1,16 @@
 <template>
     <div>
         <about-banner></about-banner>
-        <div>
-            <about-container></about-container>
-        </div>
     </div>
 </template>
 
 <script>
     import AboutBanner from "../components/about_components/aboutBanner";
-    import AboutContainer from "../components/about_components/aboutContainer";
 
     export default {
         components: {
-            AboutContainer,
-            AboutBanner},
+            AboutBanner
+        },
         name: "About"
     }
 </script>
