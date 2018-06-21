7f46d97a7da3521d224d6776b89f2adde9cf7643
Liad Golan <liad.golan.736@my.csun.edu>
2018-06-21 11:12:28 -0700

    finished writing test (but blind)

--- tests/Services/StudentProfileServiceTest.php
+++ tests/Services/StudentProfileServiceTest.php
@@ -58,11 +58,36 @@ class StudentProfileServiceTest extends TestCase
             ->withArgs([$email])
             ->andReturn(\json_encode(['people' => [
                 'profile_image' => 'thisIsAnImage',
-                'individuals_id' => 'student',
+                'individuals_id' => 'members:student',
                 'display_name' => 'Sun Ra',
             ],
         ]));
 
-        $studentService->getStudentProfile($email);
+        $this->imageCRUD
+            ->shouldReceive('getPriority')
+            ->withArgs(['student'])
+            ->andReturn(['likeness']);
+
+        $studentProfile = (object) [
+            'display_name' => 'Sun Ra',
+            'first_name' => 'Sun',
+            'last_name' => 'Ra',
+            'email' => $email,
+            'student_id' => 'members:student',
+            'members_id' => 'members:student',
+            'notes' => 'This is a note',
+            'image_priority' => 'likeness',
+        ];
+
+        $this->rosterRetriever
+            ->shouldReceive('sanitizeStudent')
+            ->withArgs([$studentProfile])
+            ->andReturn(['images' => 'evenMoreImages']);
+
+        $studentProfile->images = 'evenMoreImages';
+
+        $output = $studentService->getStudentProfile($email);
+
+        $this->assertEquals($output, \json_encode($studentProfile));
     }
 }
