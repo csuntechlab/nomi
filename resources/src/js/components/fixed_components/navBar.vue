c4698c4436df3a2952fed0263d8929de764d1762
alecnicolas <alecnmarcum@gmail.com>
2018-06-25 10:36:33 -0700

    navbar is now fixed

--- resources/src/js/components/fixed_components/navBar.vue
+++ resources/src/js/components/fixed_components/navBar.vue
@@ -1,10 +1,11 @@
 <template>
     <nav class="primary-nav">
         <back-button></back-button>
-        <div>
-            <img class="logo csun_logo" :src="this.url + '/images/csun_logo.svg'" alt="CSUN Logo">
-            <span class="sr-only">California State University, Northridge (CSUN)</span></div>
-        <img class="nomi_logo" :src="this.url + '/images/nomi.svg'">
+        <div class="logo_parent">
+            <img class="csun_logo" :src="this.url + '/images/csun_logo.svg'" alt="CSUN Logo">
+            <span class="sr-only">California State University, Northridge (CSUN)</span>
+            <img class="nomi_logo" :src="this.url + '/images/nomi.svg'">
+        </div>
     </nav>
 </template>
 
