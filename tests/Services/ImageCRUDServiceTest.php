acacc17c74f788d78bee28c90f9cc46d5a5572aa
Liad Golan <liad.golan.736@my.csun.edu>
2018-06-25 08:44:30 -0700

    did more setup for the imageCRUDServiceTest

--- tests/Services/ImageCRUDServiceTest.php
+++ tests/Services/ImageCRUDServiceTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Services;
 
 use App\Services\ImageCRUDService;
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -18,14 +19,28 @@ class ImageCRUDServiceTest extends TestCase
         $imageCRUDService = new ImageCRUDService();
 
         $student_ids = [
-            'one',
-            'two',
-            'three',
+            '1',
+            '2',
+            '3',
         ];
+
+        factory(User::class)->make([
+            'user_id' => 'members:1',
+            'ImagePriority' => 'likeness',
+        ])->save();
+
+        factory(User::class)->make([
+            'user_id' => 'members:2',
+            'ImagePriority' => 'official',
+        ])->save();
+
+        factory(User::class)->make([
+            'user_id' => 'members:3',
+        ])->save();
 
         $priorities = [
             'likeness',
-            'likeness',
+            'official',
             'likeness',
         ];
 
