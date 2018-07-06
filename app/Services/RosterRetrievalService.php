ba67dfe79f86ed1122673cac38b85c9afa555aa9
Liad Golan <liad.golan.736@my.csun.edu>
2018-07-06 13:54:13 -0700

    removed a dd

--- app/Services/RosterRetrievalService.php
+++ app/Services/RosterRetrievalService.php
@@ -85,7 +85,6 @@ class RosterRetrievalService implements RosterRetrievalContract
             $imageManager = new ImageManager(['driver' => 'imagick']);
         }
 
-        dd($student);
         $email = \str_replace('nr_', '', $student->email);
         $email = \substr($email, 0, \strpos($email, '@'));
         $imageLocation = url('images/likeness.jpg');
