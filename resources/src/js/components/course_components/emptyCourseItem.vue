05c44a94bf5141e9d296805a23e4d8d03e1222c6
alecnicolas <alecnmarcum@gmail.com>
2018-07-20 14:04:23 -0700

    empty course card now looks like the other course cards

--- resources/src/js/components/course_components/emptyCourseItem.vue
+++ resources/src/js/components/course_components/emptyCourseItem.vue
@@ -1,18 +1,16 @@
 <template>
-    <div class="panel col-lg-12 col-md-12 col-xs-12">
-         <h5 class="panel__header type--center">
+<div class="row fullscreen_width">
+    <div class="panel col-lg-12 col-md-12 col-xs-12 type--center">
+         <h5 class="panel__header mb_0">
              You are not teaching any classes this semester.
          </h5>
          <div class="panel__content">
-            <div class="row">
-                Feel free to review your past and future classes by selecting a different term.
-            </div>
+            <div class="row">Feel free to review your past and future classes by selecting a different term.</div>
             <br>
-            <div class="row">
-                Enjoy your semester!
-            </div>
+            <div class="row">Enjoy your semester!</div>
          </div>
     </div>
+</div>
 </template>
 
 <script>
