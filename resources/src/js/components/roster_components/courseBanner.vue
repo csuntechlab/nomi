48e567bf0860ab247aa6f7a182b70315905f00b5
alecnicolas <alecnmarcum@gmail.com>
2018-07-23 15:46:59 -0700

    added toolbar banner styling for roster & courses, refactored scrollTo for course banner

--- resources/src/js/components/roster_components/courseBanner.vue
+++ resources/src/js/components/roster_components/courseBanner.vue
@@ -1,7 +1,7 @@
 <template>
         <div class="tab-container">
             <ul id="scrollBar" class="tabs cf">
-                <li v-for="course in this.courses" :key="course.title" :course="course" class="tab__list">
+                <li v-for="course in this.courses" :id="course.id" :key="course.title" :course="course" class="tab__list">
                     <router-link :class="activeTab(course.id)" :to="'/class/'+course.id">
                         <div class="text_bold">
                             {{course.subject}} {{course.catalog_number}}: #{{course.section_number}}
@@ -24,11 +24,12 @@
                     return "tab__link";
             },
             setScrollBar(){
-                let scrollBar = document.getElementById('scrollBar');
-                let url = window.location.hash.split('/')[2];
-                let itemSize = document.getElementsByTagName('li')[url].offsetWidth;
-                let xPos = itemSize*url;
-                scrollBar.scrollLeft = xPos-itemSize/2;
+                let courseID = window.location.hash.split('/')[2];
+                let courseTab = document.getElementById(courseID);
+                courseTab.scrollIntoView({
+                    block:"end",
+                    behavior: "smooth"
+                });
             },
         },
         
