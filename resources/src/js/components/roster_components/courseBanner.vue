36eb659be48ba867cc7526670749b71d339c151a
alecnicolas <alecnmarcum@gmail.com>
2018-07-23 18:00:44 -0700

    dark theme implemented, cleaned up unused classes, applied themes where still missing

--- resources/src/js/components/roster_components/courseBanner.vue
+++ resources/src/js/components/roster_components/courseBanner.vue
@@ -1,15 +1,13 @@
 <template>
-        <div class="tab-container">
-            <ul id="scrollBar" class="tabs cf">
-                <li v-for="course in this.courses" :key="course.title" :course="course" class="tab__list">
-                    <router-link :class="activeTab(course.id)" :to="'/class/'+course.id">
-                        <div class="text_bold">
-                            {{course.subject}} {{course.catalog_number}}: #{{course.section_number}}
-                        </div>
-                    </router-link>
-                </li>
-            </ul>
-        </div>
+    <ul id="scrollBar" class="tabs cf">
+        <li v-for="course in this.courses" :key="course.title" :course="course" class="tab__list">
+            <router-link :class="activeTab(course.id)" :to="'/class/'+course.id">
+                <div class="text_bold">
+                    {{course.subject}} {{course.catalog_number}}: #{{course.section_number}}
+                </div>
+            </router-link>
+        </li>
+    </ul>
 </template>
 <script>
     import { mapGetters } from "vuex"
@@ -18,20 +16,20 @@
 
         methods: {
             activeTab: function (id) {
-                if(id.toString() === this.$route.params.id)
+                if (id.toString() === this.$route.params.id)
                     return "tab__link--active";
                 else
                     return "tab__link";
             },
-            setScrollBar(){
+            setScrollBar() {
                 let scrollBar = document.getElementById('scrollBar');
                 let url = window.location.hash.split('/')[2];
                 let itemSize = document.getElementsByTagName('li')[url].offsetWidth;
-                let xPos = itemSize*url;
-                scrollBar.scrollLeft = xPos-itemSize/2;
+                let xPos = itemSize * url;
+                scrollBar.scrollLeft = xPos - itemSize / 2;
             },
         },
-        
+
         mounted() {
             this.setScrollBar();
         },
