e02ac064f6cc249fd2a5da13e49aa51569de2b34
Liad Golan <liad.golan.736@my.csun.edu>
2018-08-06 15:30:33 -0700

    adds google analytics to master.blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -26,5 +26,14 @@
     </body>
     <script src="{{ asset('js/metaphor.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src=“https://www.googletagmanager.com/gtag/js?id=UA-123500967-1“></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag(‘js’, new Date());
+
+        gtag(‘config’, ‘UA-123500967-1’);
+    </script>
     @yield('scripts')
 </html>
