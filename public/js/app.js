e10dc83e57ce696efc36856068f692050ae47628
brianlinggadjaja <brianlinggadjaja@outlook.com>
2018-09-24 16:26:06 -0700

    Made course list extend full width

--- public/js/app.js
+++ public/js/app.js
@@ -19228,7 +19228,7 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "container" }, [_c("courses-container")], 1)
+  return _c("div", [_c("courses-container")], 1)
 }
 var staticRenderFns = []
 render._withStripped = true
