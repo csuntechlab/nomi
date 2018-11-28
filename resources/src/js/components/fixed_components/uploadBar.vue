939b3041e4ab1cda6dec94ba8878aea5d23ad139
BrianLinggadjaja <brianlinggadjaja@outlook.com>
2018-11-27 16:44:26 -0800

    Fixed upload response color and positioning

--- resources/src/js/components/fixed_components/uploadBar.vue
+++ resources/src/js/components/fixed_components/uploadBar.vue
@@ -1,6 +1,9 @@
 <template>
     <div v-if="this.uploadFeedback" :class="this.sticky ? 'upload-bar upload-bar--sticky' : 'upload-bar upload-bar--relative'">
         <strong>Uploaded!</strong> Photo changes will be reflected soon.
+        <div class="upload-bar-container">
+          <a href="#" class="upload-bar__close" @click="closeUpload()" >&times;</a>
+        </div>
     </div>
 </template>
 
@@ -18,5 +21,11 @@ export default {
       'uploadFeedback'
     ]),
   },
+
+  methods: {
+    closeUpload() {
+      this.$store.dispatch('stopUploadFeedback');
+    }
+  }
 };
 </script>
