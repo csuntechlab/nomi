aae30c9575a6509af83d185886f9a3b34ef256cc
jazperez <jackkiey24@gmail.com>
2019-04-10 10:32:16 -0700

    Fix css

--- resources/views/vendor/support/forms/support.blade.php
+++ resources/views/vendor/support/forms/support.blade.php
@@ -12,7 +12,7 @@
 
   <style type="text/css">
       .sub-container{
-        display: inline-block;
+        display: block;
         padding: 3%;
       }
       .submit_button {
@@ -22,8 +22,7 @@
         color: #fff;
       }
       .nomi-logo {
-        float: right;
-        height: 25%;
+        height: 75%;
       }
       .back-button {
         position: relative;
@@ -36,29 +35,29 @@
         border-color: black;
       }
       .header-bar {
-        position: fixed;
 		    height: 3rem;
 		    width: 100%;
         top: 0;
         background-color: #d00d2d;
         z-index:101;
-        display:inline-block;
+        display: block;
       }
+
     </style>
   </head>
   <body>
   <div class="header-bar">
       <img class="back-button" src="{{ asset('/images/chevron-left.svg') }}" onclick="window.history.back()" alt="Back Button">
   </div>
-    <div class="container-fluid sub-container">
+    <div class="container sub-container">
       <div class="justify-content-md-center">
         <div class="col-sm-8">
           <div class="row">
-            <div class="col-sx-6 col-sm-6 col-md-6">
-              <img class="nomi-logo" src="{{ asset('/images/apple-touch-icon.png') }}" alt="NOMI Logo; Names of Matador Individuals">
+            <div class="col-6">
+              <h2 class="m-0">Feedback Form</h2>
             </div>
-            <div class="col-sx-6 col-sm-6 col-md-6">
-              <h2>Feedback Form</h2>
+            <div class="col-6">
+              <img class="nomi-logo" src="{{ asset('/images/apple-touch-icon.png') }}" alt="NOMI Logo; Names of Matador Individuals"> 
             </div>
           </div>
           <p>Hello, Professor {{ $submitter_name }}.</p>
